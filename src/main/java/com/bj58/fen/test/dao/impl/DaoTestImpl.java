ffe94b275e6eb572fe080448f6679ec0f6e94ca2
airui2578 <airui257@163.com>
2018-12-04 17:54:42 +0800

    DEV:修改存储方式

--- src/main/java/com/bj58/fen/test/dao/impl/DaoTestImpl.java
+++ src/main/java/com/bj58/fen/test/dao/impl/DaoTestImpl.java
@@ -2,9 +2,13 @@ package com.bj58.fen.test.dao.impl;
 
 import com.bj58.fen.test.dao.DaoTest;
 import com.bj58.fen.test.entity.Person;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.springframework.orm.hibernate5.support.HibernateDaoSupport;
 import org.springframework.stereotype.Repository;
 
+import javax.annotation.Resource;
 import java.io.Serializable;
 
 /**
@@ -13,18 +17,16 @@ import java.io.Serializable;
 @Repository("daoTest")
 public class DaoTestImpl extends HibernateDaoSupport implements DaoTest {
 
-//    @Resource
-//    SessionFactory sessionFactory;
+    @Resource
+    SessionFactory sessionFactory;
 
     @Override
     public void save(Person person) {
-//        Session session = sessionFactory.openSession();
-//        Transaction transaction = session.beginTransaction();
-//        session.save(person);
-//        transaction.commit();
-//        session.close();
-
-        getHibernateTemplate().save(person);
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(person);
+        transaction.commit();
+        session.close();
     }
 
     @Override
